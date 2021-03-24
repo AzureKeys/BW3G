f7cef0a1256887970ac6ce2ff4ff6034c0207fc8
U-LAPTOP-CDPSN53K\mattg <mattgmercier@gmail.com>
2021-03-24 12:48:09 -0500

    Cleanup Map files

--- data/maps/outdoor_sprites.asm
+++ data/maps/outdoor_sprites.asm
@@ -3,32 +3,8 @@
 
 OutdoorSprites:
 ; entries correspond to map groups
-	; dw OlivineGroupSprites
-	; dw MahoganyGroupSprites
 	dw DungeonsGroupSprites
-	; dw EcruteakGroupSprites
-	; dw BlackthornGroupSprites
-	; dw CinnabarGroupSprites
-	; dw CeruleanGroupSprites
-	; dw AzaleaGroupSprites
-	; dw LakeOfRageGroupSprites
-	; dw VioletGroupSprites
-	; dw GoldenrodGroupSprites
-	; dw VermilionGroupSprites
-	; dw PalletGroupSprites
-	; dw PewterGroupSprites
-	; dw FastShipGroupSprites
-	; dw IndigoGroupSprites
-	; dw FuchsiaGroupSprites
-	; dw LavenderGroupSprites
-	; dw SilverGroupSprites
 	dw CableClubGroupSprites
-	; dw CeladonGroupSprites
-	; dw CianwoodGroupSprites
-	; dw ViridianGroupSprites
-	; dw NewBarkGroupSprites
-	; dw SaffronGroupSprites
-	; dw CherrygroveGroupSprites
 	dw HumilauGroupSprites
 	dw LacunosaGroupSprites
 	dw UndellaGroupSprites
@@ -48,139 +24,6 @@ OutdoorSprites:
 	dw MistraltonGroupSprites
 	dw OpelucidGroupSprites
 	dw PkmnLeagueGroupSprites
-
-; Route1 and ViridianCity are connected
-; Route2 and PewterCity are connected
-; PalletTown and Route21 are connected
-;PalletGroupSprites:
-; Route1, PalletTown
-;ViridianGroupSprites:
-; Route2, Route22, ViridianCity
-;PewterGroupSprites:
-; Route3, PewterCity
-; CinnabarGroupSprites:
-; ; Route19, Route20, Route21, CinnabarIsland
-	; db SPRITE_TEACHER
-	; db SPRITE_FISHER
-	; db SPRITE_YOUNGSTER
-	; db SPRITE_BLUE
-	; db SPRITE_GRAMPS
-	; db SPRITE_BUG_CATCHER
-	; db SPRITE_COOLTRAINER_F
-	; db SPRITE_SWIMMER_GIRL
-	; db SPRITE_SWIMMER_GUY
-	; ; limit of 9 walking sprites
-	; db SPRITE_POKE_BALL
-	; db SPRITE_FRUIT_TREE
-	; db 0 ; end
-
-; ; CeruleanCity and Route5 are connected
-; CeruleanGroupSprites:
-; ; Route4, Route9, Route10North, Route24, Route25, CeruleanCity
-; SaffronGroupSprites:
-; ; Route5, SaffronCity
-	; db SPRITE_COOLTRAINER_M
-	; db SPRITE_SUPER_NERD
-	; db SPRITE_COOLTRAINER_F
-	; db SPRITE_FISHER
-	; db SPRITE_YOUNGSTER
-	; db SPRITE_LASS
-	; db SPRITE_POKEFAN_M
-	; db SPRITE_ROCKET
-	; db SPRITE_MISTY
-	; ; limit of 9 walking sprites
-	; db SPRITE_POKE_BALL
-	; db SPRITE_SLOWPOKE
-	; db 0 ; end
-
-; VermilionGroupSprites:
-; ; Route6, Route11, VermilionCity
-	; db SPRITE_POKEFAN_M
-	; db SPRITE_GRAMPS
-	; db SPRITE_YOUNGSTER
-	; db SPRITE_TEACHER
-	; db SPRITE_SUPER_NERD
-	; ; 5 of max 9 walking sprites
-	; db SPRITE_BIG_SNORLAX
-	; db SPRITE_MACHOP
-	; db SPRITE_POKE_BALL
-	; db SPRITE_FRUIT_TREE
-	; db 0 ; end
-
-; CeladonGroupSprites:
-; ; Route7, Route16, Route17, CeladonCity
-	; db SPRITE_FISHER
-	; db SPRITE_TEACHER
-	; db SPRITE_GRAMPS
-	; db SPRITE_YOUNGSTER
-	; db SPRITE_LASS
-	; db SPRITE_BIKER
-	; ; 6 of max 9 walking sprites
-	; db SPRITE_POLIWAG
-	; db SPRITE_POKE_BALL
-	; db SPRITE_FRUIT_TREE
-	; db 0 ; end
-
-; ; Route12 and Route13 are connected
-; LavenderGroupSprites:
-; ; Route8, Route12, Route10South, LavenderTown
-; FuchsiaGroupSprites:
-; ; Route13, Route14, Route15, Route18, FuchsiaCity
-	; db SPRITE_POKEFAN_M
-	; db SPRITE_GRAMPS
-	; db SPRITE_YOUNGSTER
-	; db SPRITE_FISHER
-	; db SPRITE_TEACHER
-	; db SPRITE_SUPER_NERD
-	; db SPRITE_BIKER
-	; ; 7 of max 9 walking sprites
-	; db SPRITE_POKE_BALL
-	; db SPRITE_FRUIT_TREE
-	; db 0 ; end
-
-; IndigoGroupSprites:
-; ; Route23
-	; ; 0 of max 9 walking sprites
-	; db 0 ; end
-
-; ; Route29 and CherrygroveCity are connected
-; NewBarkGroupSprites:
-; ; Route26, Route27, Route29, NewBarkTown
-; CherrygroveGroupSprites:
-; ; Route30, Route31, CherrygroveCity
-	; db SPRITE_SILVER
-	; db SPRITE_TEACHER
-	; db SPRITE_FISHER
-	; db SPRITE_COOLTRAINER_M
-	; db SPRITE_YOUNGSTER
-	; db SPRITE_MONSTER
-	; db SPRITE_GRAMPS
-	; db SPRITE_BUG_CATCHER
-	; db SPRITE_COOLTRAINER_F
-	; ; limit of 9 walking sprites
-	; db SPRITE_POKE_BALL
-	; db SPRITE_FRUIT_TREE
-	; db 0 ; end
-
-; ; Route37 and EcruteakCity are connected
-; VioletGroupSprites:
-; ; Route32, Route35, Route36, Route37, VioletCity
-; EcruteakGroupSprites:
-; ; EcruteakCity
-	; db SPRITE_FISHER
-	; db SPRITE_LASS
-	; db SPRITE_OFFICER
-	; db SPRITE_GRAMPS
-	; db SPRITE_YOUNGSTER
-	; db SPRITE_COOLTRAINER_M
-	; db SPRITE_BUG_CATCHER
-	; db SPRITE_SUPER_NERD
-	; ; 8 of max 9 walking sprites
-	; db SPRITE_WEIRD_TREE ; variable sprite: becomes SPRITE_SUDOWOODO and SPRITE_TWIN
-	; db SPRITE_POKE_BALL
-	; db SPRITE_FRUIT_TREE
-	; db SPRITE_SUICUNE
-	db 0 ; end
 
 HumilauGroupSprites:
 ; Humilau City, Route 22, Route 21
@@ -451,94 +294,6 @@ PkmnLeagueGroupSprites:
 	db SPRITE_POKE_BALL
 	db 0 ; end
 
-; GoldenrodGroupSprites:
-; ; Route34, GoldenrodCity
-	; db SPRITE_GRAMPS
-	; db SPRITE_YOUNGSTER
-	; db SPRITE_OFFICER
-	; db SPRITE_POKEFAN_M
-	; db SPRITE_COOLTRAINER_F
-	; db SPRITE_ROCKET
-	; db SPRITE_LASS
-	; ; 7 of max 9 walking sprites
-	; db SPRITE_DAY_CARE_MON_1
-	; db SPRITE_DAY_CARE_MON_2
-	; db SPRITE_POKE_BALL
-	; db 0 ; end
-
-; ; OlivineCity and Route40 are connected
-; OlivineGroupSprites:
-; ; Route38, Route39, OlivineCity
-; CianwoodGroupSprites:
-; ; Route40, Route41, CianwoodCity, BattleTowerOutside
-	; db SPRITE_OLIVINE_RIVAL ; variable sprite: becomes SPRITE_SILVER and SPRITE_SWIMMER_GUY
-	; db SPRITE_POKEFAN_M
-	; db SPRITE_LASS
-	; db SPRITE_BUENA
-	; db SPRITE_SWIMMER_GIRL
-	; db SPRITE_SAILOR
-	; db SPRITE_POKEFAN_F
-	; db SPRITE_SUPER_NERD
-	; ; 8 of max 9 walking sprites
-	; db SPRITE_TAUROS
-	; db SPRITE_FRUIT_TREE
-	; db SPRITE_ROCK
-	; db SPRITE_STANDING_YOUNGSTER ; non-walking version of SPRITE_YOUNGSTER
-	; db SPRITE_SUICUNE
-	; db 0 ; end
-
-; MahoganyGroupSprites:
-; ; Route42, Route44, MahoganyTown
-	; db SPRITE_GRAMPS
-	; db SPRITE_YOUNGSTER
-	; db SPRITE_LASS
-	; db SPRITE_SUPER_NERD
-	; db SPRITE_COOLTRAINER_M
-	; db SPRITE_POKEFAN_M
-	; db SPRITE_COOLTRAINER_F
-	; db SPRITE_FISHER
-	; ; 8 of max 9 walking sprites
-	; db SPRITE_FRUIT_TREE
-	; db SPRITE_POKE_BALL
-	; db SPRITE_SUICUNE
-	; db 0 ; end
-
-; LakeOfRageGroupSprites:
-; ; Route43, LakeOfRage
-	; db SPRITE_LANCE
-	; db SPRITE_GRAMPS
-	; db SPRITE_SUPER_NERD
-	; db SPRITE_COOLTRAINER_F
-	; db SPRITE_FISHER
-	; db SPRITE_COOLTRAINER_M
-	; db SPRITE_LASS
-	; db SPRITE_YOUNGSTER
-	; ; 8 of max 9 walking sprites
-	; db SPRITE_GYARADOS
-	; db SPRITE_FRUIT_TREE
-	; db SPRITE_POKE_BALL
-	; db 0 ; end
-
-; BlackthornGroupSprites:
-; ; Route45, Route46, BlackthornCity
-	; db SPRITE_GRAMPS
-	; db SPRITE_YOUNGSTER
-	; db SPRITE_LASS
-	; db SPRITE_SUPER_NERD
-	; db SPRITE_COOLTRAINER_M
-	; db SPRITE_POKEFAN_M
-	; db SPRITE_BLACK_BELT
-	; db SPRITE_COOLTRAINER_F
-	; ; 8 of max 9 walking sprites
-	; db SPRITE_FRUIT_TREE
-	; db SPRITE_POKE_BALL
-	; db 0 ; end
-
-; SilverGroupSprites:
-; ; Route28, SilverCaveOutside
-	; ; 0 of max 9 walking sprites
-	; db 0 ; end
-
 DungeonsGroupSprites:
 ; Pinwheel Forest, Giant Chasm B1F
 	db SPRITE_LASS
@@ -553,19 +308,6 @@ DungeonsGroupSprites:
 	; 8 of max 9 walking sprites
 	db SPRITE_POKE_BALL
 	db 0 ; end
-
-; FastShipGroupSprites:
-; ; OlivinePort, VermilionPort, MountMoonSquare, TinTowerRoof
-	; db SPRITE_SAILOR
-	; db SPRITE_FISHING_GURU
-	; db SPRITE_SUPER_NERD
-	; db SPRITE_COOLTRAINER_F
-	; db SPRITE_YOUNGSTER
-	; ; 5 of max 9 walking sprites
-	; db SPRITE_HO_OH
-	; db SPRITE_FAIRY
-	; db SPRITE_ROCK
-	; db 0 ; end
 
 CableClubGroupSprites:
 ; (no outdoor maps)
--- data/maps/scenes.asm
+++ data/maps/scenes.asm
@@ -9,79 +9,7 @@ MapScenes::
 	scene_var TRADE_CENTER,                                wTradeCenterSceneID
 	scene_var COLOSSEUM,                                   wColosseumSceneID
 	scene_var TIME_CAPSULE,                                wTimeCapsuleSceneID
-	; scene_var POWER_PLANT,                                 wPowerPlantSceneID
-	; scene_var CERULEAN_GYM,                                wCeruleanGymSceneID
-	; scene_var ROUTE_25,                                    wRoute25SceneID
-	; scene_var TRAINER_HOUSE_B1F,                           wTrainerHouseB1FSceneID
-	; scene_var VICTORY_ROAD_GATE,                           wVictoryRoadGateSceneID
-	; scene_var SAFFRON_MAGNET_TRAIN_STATION,                wSaffronMagnetTrainStationSceneID
-	; scene_var ROUTE_16_GATE,                               wRoute16GateSceneID
-	; scene_var ROUTE_17_ROUTE_18_GATE,                      wRoute17Route18GateSceneID
-	; scene_var INDIGO_PLATEAU_POKECENTER_1F,                wIndigoPlateauPokecenter1FSceneID
-	; scene_var WILLS_ROOM,                                  wWillsRoomSceneID
-	; scene_var KOGAS_ROOM,                                  wKogasRoomSceneID
-	; scene_var BRUNOS_ROOM,                                 wBrunosRoomSceneID
-	; scene_var KARENS_ROOM,                                 wKarensRoomSceneID
-	; scene_var LANCES_ROOM,                                 wLancesRoomSceneID
-	; scene_var HALL_OF_FAME,                                wHallOfFameSceneID
-	; scene_var ROUTE_27,                                    wRoute27SceneID
-	; scene_var NEW_BARK_TOWN,                               wNewBarkTownSceneID
-	; scene_var ELMS_LAB,                                    wElmsLabSceneID
 	scene_var PLAYERS_HOUSE_1F,                            wPlayersHouse1FSceneID
-	; scene_var ROUTE_29,                                    wRoute29SceneID
-	; scene_var CHERRYGROVE_CITY,                            wCherrygroveCitySceneID
-	; scene_var MR_POKEMONS_HOUSE,                           wMrPokemonsHouseSceneID
-	; scene_var ROUTE_32,                                    wRoute32SceneID
-	; scene_var ROUTE_35_NATIONAL_PARK_GATE,                 wRoute35NationalParkGateSceneID
-	; scene_var ROUTE_36,                                    wRoute36SceneID
-	; scene_var ROUTE_36_NATIONAL_PARK_GATE,                 wRoute36NationalParkGateSceneID
-	; scene_var AZALEA_TOWN,                                 wAzaleaTownSceneID
-	; scene_var GOLDENROD_GYM,                               wGoldenrodGymSceneID
-	; scene_var GOLDENROD_MAGNET_TRAIN_STATION,              wGoldenrodMagnetTrainStationSceneID
-	; scene_var GOLDENROD_POKECENTER_1F,                     wGoldenrodPokecenter1FSceneID
-	; scene_var OLIVINE_CITY,                                wOlivineCitySceneID
-	; scene_var ROUTE_34,                                    wRoute34SceneID ; unused
-	; scene_var ROUTE_34_ILEX_FOREST_GATE,                   wRoute34IlexForestGateSceneID ; unused
-	; scene_var ECRUTEAK_TIN_TOWER_ENTRANCE,                 wEcruteakTinTowerEntranceSceneID
-	; scene_var WISE_TRIOS_ROOM,                             wWiseTriosRoomSceneID
-	; scene_var ECRUTEAK_POKECENTER_1F,                      wEcruteakPokecenter1FSceneID
-	; scene_var ECRUTEAK_GYM,                                wEcruteakGymSceneID
-	; scene_var MAHOGANY_TOWN,                               wMahoganyTownSceneID
-	; scene_var ROUTE_42,                                    wRoute42SceneID
-	; scene_var CIANWOOD_CITY,                               wCianwoodCitySceneID
-	; scene_var BATTLE_TOWER_1F,                             wBattleTower1FSceneID
-	; scene_var BATTLE_TOWER_BATTLE_ROOM,                    wBattleTowerBattleRoomSceneID
-	; scene_var BATTLE_TOWER_ELEVATOR,                       wBattleTowerElevatorSceneID
-	; scene_var BATTLE_TOWER_HALLWAY,                        wBattleTowerHallwaySceneID
-	; scene_var BATTLE_TOWER_OUTSIDE,                        wBattleTowerOutsideSceneID ; unused
-	; scene_var ROUTE_43_GATE,                               wRoute43GateSceneID
-	; scene_var MOUNT_MOON,                                  wMountMoonSceneID
-	; scene_var SPROUT_TOWER_3F,                             wSproutTower3FSceneID
-	; scene_var TIN_TOWER_1F,                                wTinTower1FSceneID
-	; scene_var BURNED_TOWER_1F,                             wBurnedTower1FSceneID
-	; scene_var BURNED_TOWER_B1F,                            wBurnedTowerB1FSceneID
-	; scene_var RADIO_TOWER_5F,                              wRadioTower5FSceneID
-	; scene_var RUINS_OF_ALPH_OUTSIDE,                       wRuinsOfAlphOutsideSceneID
-	; scene_var RUINS_OF_ALPH_RESEARCH_CENTER,               wRuinsOfAlphResearchCenterSceneID
-	; scene_var RUINS_OF_ALPH_HO_OH_CHAMBER,                 wRuinsOfAlphHoOhChamberSceneID
-	; scene_var RUINS_OF_ALPH_KABUTO_CHAMBER,                wRuinsOfAlphKabutoChamberSceneID
-	; scene_var RUINS_OF_ALPH_OMANYTE_CHAMBER,               wRuinsOfAlphOmanyteChamberSceneID
-	; scene_var RUINS_OF_ALPH_AERODACTYL_CHAMBER,            wRuinsOfAlphAerodactylChamberSceneID
-	; scene_var RUINS_OF_ALPH_INNER_CHAMBER,                 wRuinsOfAlphInnerChamberSceneID
-	; scene_var MAHOGANY_MART_1F,                            wMahoganyMart1FSceneID
-	; scene_var TEAM_ROCKET_BASE_B1F,                        wTeamRocketBaseB1FSceneID
-	; scene_var TEAM_ROCKET_BASE_B2F,                        wTeamRocketBaseB2FSceneID
-	; scene_var TEAM_ROCKET_BASE_B3F,                        wTeamRocketBaseB3FSceneID
-	; scene_var GOLDENROD_UNDERGROUND_SWITCH_ROOM_ENTRANCES, wGoldenrodUndergroundSwitchRoomEntrancesSceneID
-	; scene_var SILVER_CAVE_ROOM_3,                          wSilverCaveRoom3SceneID ; unused
-	; scene_var VICTORY_ROAD,                                wVictoryRoadSceneID
-	; scene_var DRAGONS_DEN_B1F,                             wDragonsDenB1FSceneID
-	; scene_var DRAGON_SHRINE,                               wDragonShrineSceneID
-	; scene_var OLIVINE_PORT,                                wOlivinePortSceneID
-	; scene_var VERMILION_PORT,                              wVermilionPortSceneID
-	; scene_var FAST_SHIP_1F,                                wFastShip1FSceneID
-	; scene_var FAST_SHIP_B1F,                               wFastShipB1FSceneID
-	; scene_var MOUNT_MOON_SQUARE,                           wMountMoonSquareSceneID
 	scene_var MOBILE_TRADE_ROOM,                           wMobileTradeRoomSceneID
 	scene_var MOBILE_BATTLE_ROOM,                          wMobileBattleRoomSceneID
 	scene_var MARLONS_HOUSE,                               wMarlonsHouseSceneID
