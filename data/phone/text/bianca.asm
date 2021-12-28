20406de79dcf3071de5b4cc4b588a4d82d9b5ae7
U-LAPTOP-CDPSN53K\mattg <mattgmercier@gmail.com>
2021-12-28 16:47:30 -0600

    Call Bianca for help

--- data/phone/text/bianca.asm
+++ data/phone/text/bianca.asm
@@ -1,20 +1,308 @@
-BiancaDexCheckText:
-	text "Oh, <PLAY_G>!"
-	line "It's so good to"
-	cont "hear from you!"
+BiancaPhoneGreetingText:
+	text "Oh, hi <PLAY_G>!"
 	
-	para "How is your #-"
-	line "DEX coming?"
-
+	para "Did you need some"
+	line "help on your"
+	cont "journey?"
+	
 	para "Let's see…"
 	done
-
-BiancaGoodbyeText:
-	text "If you're in the"
-	line "area, I hope you"
-	cont "come by to visit!"
+	
+BiancaPhoneGoToUndellaText:
+	text "MARLON's gone to"
+	line "UNDELLA TOWN."
+	
+	para "You can get there"
+	line "by going south on"
+	cont "ROUTE 13."
 	done
 	
+BiancaPhoneHumilauGymText:
+	text "Your first major"
+	line "challenge as a"
+	
+	para "trainer will be to"
+	line "take on MARLON in"
+	
+	para "the HUMILAU CITY"
+	line "GYM!"
+	done
+	
+BiancaPhoneReversalText:
+	text "In UNDELLA TOWN,"
+	line "you can find the"
+	
+	para "entrance to"
+	line "REVERSAL MOUNTAIN."
+	
+	para "Make your way"
+	line "through to find"
+	
+	para "new places to"
+	line "explore!"
+	done
+	
+BiancaPhoneGoToLentimasText:
+	text "If you go through"
+	line "REVERSAL MOUNTAIN,"
+	
+	para "you can find a"
+	line "place called"
+	cont "LENTIMAS TOWN."
+	
+	para "You'll find a new"
+	line "challenge waiting"
+	cont "for you there."
+	done
+	
+BiancaPhoneGetCutText:
+	text "Near LENTIMAS TOWN"
+	line "is a place called"
+	cont "LOSTLORN FOREST."
+	
+	para "If you can't make"
+	line "your way through,"
+	
+	para "there may be some-"
+	line "thing that can"
+	
+	para "help in the old"
+	line "house outside of"
+	cont "town."
+	done
+	
+BiancaPhoneGoToCasteliaText:
+	text "Through LOSTLORN"
+	line "FOREST, you can"
+	
+	para "find the way to"
+	line "NIMBASA CITY."
+	
+	para "It's a big city,"
+	line "but if you keep"
+	
+	para "going south, you"
+	line "can reach CASTELIA"
+	
+	para "CITY, which is"
+	line "even bigger!"
+	done
+	
+BiancaPhoneCasteliaGymText:
+	text "CASTELIA CITY has"
+	line "a lot to see, but"
+	
+	para "as a #MON"
+	line "trainer, I'm sure"
+	
+	para "nothing is quite"
+	line "as exciting as the"
+	cont "#MON GYM!"
+	done
+	
+BiancaPhoneNimbasaParkText:
+	text "TEAM PLASMA is"
+	line "back? That can't"
+	cont "be good."
+	
+	para "CHEREN's gone to"
+	line "the PARK in"
+	cont "NIMBASA CITY."
+	
+	para "You should go help"
+	line "him out, <PLAY_G>!"
+	done
+	
+BiancaPhoneAspertiaText:
+	text "Oh, CHEREN gave"
+	line "you a RAIL PASS?"
+	
+	para "You can use that"
+	line "to take the SUBWAY"
+	cont "in NIMBASA CITY!"
+	done
+	
+BiancaPhoneComplexText:
+	text "CHEREN's gone to"
+	line "VIRBANK COMPLEX to"
+	
+	para "investigate a lead"
+	line "on TEAM PLASMA."
+	
+	para "You should join"
+	line "him, <PLAY_G>!"
+	done
+	
+BiancaPhoneNuvemaText:
+	text "The construction"
+	line "on the SKYARROW"
+	
+	para "BRIDGE is finally"
+	line "finished!"
+	
+	para "If you cross the"
+	line "bridge from"
+	cont "CASTELIA CITY,"
+	
+	para "you can pass"
+	line "through some towns"
+	
+	para "to reach NUVEMA"
+	line "TOWN, where my lab"
+	cont "is located!"
+	done
+	
+BiancaPhoneP2LabText:
+	text "PROF.JUNIPER's"
+	line "gone to check out"
+	
+	para "some secret lab om"
+	line "ROUTE 17. You can"
+	
+	para "get there by SURF-"
+	line "ing across the"
+	
+	para "water on ROUTE 1."
+	line "You'll need the"
+	
+	para "GYM BADGE from"
+	line "STRIATON CITY to"
+	cont "use it, though."
+	done
+	
+BiancaPhoneGoToPWTText:
+	text "Are you coming to"
+	line "the #MON WORLD"
+	cont "TOURNAMENT?"
+	
+	para "You can get there"
+	line "by passing through"
+	
+	para "the SEWERS in"
+	line "CASTELIA CITY."
+	
+	para "If your way is"
+	line "blocked, try"
+	
+	para "searching the"
+	line "SEWERS for some-"
+
+	para "thing that can"
+	line "help."
+	
+	para "And make sure you"
+	line "take on CHEREN's"
+	
+	para "GYM in ASPERTIA"
+	line "CITY if you"
+	cont "haven't yet!"
+	done
+	
+BiancaPhoneFinishPWTText:
+	text "The #MON WORLD"
+	line "TOURNAMENT is"
+	cont "underway!"
+	
+	para "You should enter"
+	line "and see how far"
+	cont "you can go!"
+	done
+	
+BiancaPhoneMistraltonGymText:
+	text "If you go west of"
+	line "DRIFTVEIL CITY,"
+	
+	para "through ROUTE 6,"
+	line "you can find the"
+	
+	para "entrance to"
+	line "CHARGESTONE CAVE."
+	
+	para "MISTRALTON CITY is"
+	line "on the other side."
+	
+	para "You should take on"
+	line "the GYM there!"
+	done
+	
+BiancaPhoneSeasideCaveText:
+	text "CHEREN found a"
+	line "group of TEAM"
+	
+	para "PLAMSA going to"
+	line "a cave near your"
+	cont "hometown!"
+	
+	para "You can get to"
+	line "this cave by SURF-"
+	
+	para "ing from HUMILAU"
+	line "CITY."
+	done
+	
+BiancaPhoneVillageBridgeText:
+	text "The VILLAGE BRIDGE"
+	line "is open to travel"
+	cont "again, <PLAY_G>!"
+	
+	para "You can get there"
+	line "by going west of"
+	cont "LACUNOSA TOWN."
+	done
+	
+BiancaPhoneFindDraydenText:
+	text "It looks like the"
+	line "OPELUCID CITY GYM"
+	
+	para "LEADER has gone to"
+	line "ICIRRUS CITY. You"
+	
+	para "can get there by"
+	line "going west."
+	
+	para "I wonder what's"
+	line "going on there…"
+	done
+	
+BiancaPhoneOpelucidGymText:
+	text "Now that DRAYDEN"
+	line "has returned, you"
+	
+	para "can finally take"
+	line "on the OPELUCID"
+	cont "CITY GYM!"
+	done
+	
+BiancaPhoneVictoryRoadText:
+	text "Wow, <PLAY_G>,"
+	line "you've earned all"
+	cont "8 GYM BADGES!"
+	
+	para "Now the only thing"
+	line "left for you to do"
+	
+	para "is to take on the"
+	line "#MON LEAGUE!"
+	
+	para "Go through ROUTE"
+	line "23 and go through"
+	
+	para "VICTORY ROAD, and"
+	line "you'll be there!"
+	
+	para "Good luck,"
+	line "<PLAY_G>!"
+	done
+	
+BiancaPhoneDoneText:
+	text "Call again if you"
+	line "need any more"
+	cont "advice!"
+	
+	para "See you,"
+	line "<PLAY_G>!"
+	done
+
 BiancaPhoneIntroText:
 	text "Hi, is this"
 	line "<PLAY_G>?"
@@ -26,11 +314,12 @@ BiancaPhoneIntroText:
 	line "you the #DEX"
 	cont "I sent."
 	
-	para "If you want an"
-	line "update on your"
+	para "If you are ever"
+	line "unsure on where to"
 	
-	para "#DEX progress,"
-	line "give me a call."
+	para "go next, give me a"
+	line "call, and I'll try"
+	cont "to help!"
 	
 	para "And if you're ever"
 	line "near NUVEMA TOWN,"
--- engine/phone/scripts/bianca.asm
+++ engine/phone/scripts/bianca.asm
@@ -1,8 +1,124 @@
 BiancaPhoneCalleeScript:
-	farwritetext BiancaDexCheckText
-	waitbutton
-	special ProfOaksPCBoot
-	farwritetext BiancaGoodbyeText
+	farwritetext BiancaPhoneGreetingText
+	buttonsound
+	checkflag ENGINE_RISINGBADGE
+	iftrue .victory_road
+	; checkmapscene DRAGONSPIRAL_TOWER_ROOF, SCENE_FINISHED
+	; iftrue .opelucid_gym
+	checkmapscene OPELUCID_CITY, SCENE_FINISHED
+	iftrue .find_drayden
+	checkmapscene SEASIDE_CAVE_CHAMBER, SCENE_FINISHED
+	iftrue .village_bridge
+	checkflag ENGINE_GLACIERBADGE
+	iftrue .seaside_cave
+	checkmapscene DRIFTVEIL_SHELTER, SCENE_FINISHED
+	iftrue .mistralton_gym
+	checkmapscene PWT_OUTSIDE, SCENE_FINISHED
+	iftrue .finish_pwt
+	checkevent EVENT_BEAT_P2_LAB
+	iftrue .go_to_pwt
+	checkmapscene NUVEMA_LAB, SCENE_FINISHED
+	iftrue .p2_lab
+	checkmapscene CASTELIA_BRIDGE_GATE, SCENE_FINISHED
+	iftrue .nuvema
+	checkmapscene FLOCCESY_TOWN, SCENE_FINISHED
+	iftrue .complex
+	checkmapscene NIMBASA_PARK_BASEMENT, SCENE_NIMBASA_PARK_BASEMENT_NOTHING
+	iftrue .aspertia
+	checkflag ENGINE_PLAINBADGE
+	iftrue .nimbasa_park
+	checkevent EVENT_BIANCA_CASTELIA_CALL
+	iftrue .castelia_gym
+	checkmapscene LOSTLORN_FOREST, SCENE_LOSTLORN_NOTHING
+	iftrue .go_to_castelia
+	checkflag ENGINE_HIVEBADGE
+	iftrue .get_cut
+	checkmapscene UNDELLA_TOWN, SCENE_UNDELLA_TOWN_NOTHING
+	iftrue .go_to_lentimas
+	checkflag ENGINE_ZEPHYRBADGE
+	iftrue .reversal
+	checkmapscene UNDELLA_TOWN, SCENE_UNDELLA_TOWN_CANT_LEAVE
+	iftrue .humilau_gym
+	farwritetext BiancaPhoneGoToUndellaText
+.done
+	buttonsound
+	farwritetext BiancaPhoneDoneText
+	end
+	
+.humilau_gym
+	farwritetext BiancaPhoneHumilauGymText
+	jump .done
+	
+.reversal
+	farwritetext BiancaPhoneReversalText
+	jump .done
+	
+.go_to_lentimas
+	farwritetext BiancaPhoneGoToLentimasText
+	jump .done
+	
+.get_cut
+	farwritetext BiancaPhoneGetCutText
+	jump .done
+	
+.go_to_castelia
+	farwritetext BiancaPhoneGoToCasteliaText
+	jump .done
+	
+.castelia_gym
+	farwritetext BiancaPhoneCasteliaGymText
+	jump .done
+	
+.nimbasa_park
+	farwritetext BiancaPhoneNimbasaParkText
+	jump .done
+	
+.aspertia
+	farwritetext BiancaPhoneAspertiaText
+	jump .done
+	
+.complex
+	farwritetext BiancaPhoneComplexText
+	jump .done
+	
+.nuvema
+	farwritetext BiancaPhoneNuvemaText
+	jump .done
+	
+.p2_lab
+	farwritetext BiancaPhoneP2LabText
+	jump .done
+	
+.go_to_pwt
+	farwritetext BiancaPhoneGoToPWTText
+	jump .done
+	
+.finish_pwt
+	farwritetext BiancaPhoneFinishPWTText
+	jump .done
+	
+.mistralton_gym
+	farwritetext BiancaPhoneMistraltonGymText
+	jump .done
+	
+.seaside_cave
+	farwritetext BiancaPhoneSeasideCaveText
+	jump .done
+	
+.village_bridge
+	farwritetext BiancaPhoneVillageBridgeText
+	jump .done
+	
+.find_drayden
+	farwritetext BiancaPhoneFindDraydenText
+	jump .done
+	
+.opelucid_gym
+	farwritetext BiancaPhoneOpelucidGymText
+	jump .done
+	
+.victory_road
+	farwritetext BiancaPhoneVictoryRoadText
 	end
 
 BiancaPhoneCallerScript:
