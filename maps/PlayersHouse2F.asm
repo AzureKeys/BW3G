7b78b96d2755fd2e541acb1f5bed7a36ebc9043b
U-LAPTOP-CDPSN53K\mattg <mattgmercier@gmail.com>
2022-09-05 17:21:56 -0500

    Fixed Trainer Name Overflow

--- maps/PlayersHouse2F.asm
+++ maps/PlayersHouse2F.asm
@@ -149,7 +149,7 @@ PlayersHouse2F_MapEvents:
 
 	db 1 ; warp events
 	warp_event  7,  0, PLAYERS_HOUSE_1F, 3
-	;warp_event  7,  0, PWT_OUTSIDE, 1
+	;warp_event  7,  0, FLOCCESY_RANCH, 1
 
 	db 0 ; coord events
 
