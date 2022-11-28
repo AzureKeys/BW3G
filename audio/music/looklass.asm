b2e6840ced3d4493a7b8131baa16a2bb627978b7
U-LAPTOP-CDPSN53K\mattg <mattgmercier@gmail.com>
2022-11-27 21:29:14 -0600

    New Lass Encounter Theme

--- audio/music/looklass.asm
+++ audio/music/looklass.asm
@@ -1,114 +1,583 @@
+;Coverted using MIDI2ASM
+;Code by TriteHexagon
+;Version 5.0.1 (7-Feb-2021)
+;Visit github.com/TriteHexagon/Midi2ASM-Converter for up-to-date versions.
+
+; ============================================================================================================
+
 Music_LookLass:
-	musicheader 4, 1, Music_LookLass_Ch1
+	musicheader 3, 1, Music_LookLass_Ch1
 	musicheader 1, 2, Music_LookLass_Ch2
 	musicheader 1, 3, Music_LookLass_Ch3
-	musicheader 1, 4, Music_LookLass_Ch4
 
 Music_LookLass_Ch1:
-	tempo 132
 	volume $77
-	tone $0001
 	dutycycle $2
-	notetype $c, $c7
+	notetype 12, $a7
+	tempo 111
+;Bar 1
+Music_LookLass_Ch1_loop:
+	octave 5
+	note F_, 1
+	note __, 1
 	octave 3
-	note B_, 1
+	note F_, 1
 	note __, 1
 	octave 4
-	note B_, 14
-	stereopanning $f
-	dutycycle $0
-Music_LookLass_branch_ed7bd:
-	intensity $a2
+	note F_, 1
+	note __, 1
 	octave 3
-	note E_, 4
-	intensity $a1
-	note E_, 4
-	note E_, 4
-	note E_, 2
-	intensity $a3
-	note B_, 2
-	loopchannel 0, Music_LookLass_branch_ed7bd
+	note F_, 1
+	note __, 1
+	octave 4
+	note G_, 1
+	note __, 1
+	note F_, 1
+	note __, 1
+	note A_, 1
+	note __, 1
+	notetype 6, $a7
+	note F_, 1
+	note __, 3
+	notetype 12, $a7
+	octave 5
+;Bar 2
+	note C_, 1
+	note __, 1
+	octave 4
+	note A#, 1
+	note __, 1
+	note A_, 1
+	note __, 1
+	note A#, 1
+	note __, 1
+	octave 5
+	note D#, 1
+	note __, 1
+	note D_, 1
+	note __, 1
+	note D#, 1
+	note __, 1
+	note F_, 1
+	note __, 1
+	octave 4
+;Bar 3
+	note A_, 1
+	note __, 1
+	note G_, 1
+	note __, 1
+	note F#, 1
+	note __, 1
+	note G_, 1
+	note __, 1
+	octave 5
+	note D_, 1
+	note __, 1
+	note C_, 1
+	note __, 1
+	octave 4
+	note B_, 1
+	note __, 1
+	octave 5
+	note C_, 1
+	note __, 1
+	octave 4
+;Bar 4
+	note G_, 1
+	note __, 1
+	note F_, 1
+	note __, 1
+	note E_, 1
+	note __, 1
+	note F_, 1
+	note __, 1
+	note A#, 1
+	note __, 1
+	note A_, 1
+	note __, 1
+	note G#, 1
+	note __, 1
+	note A_, 1
+	note __, 1
+	octave 5
+;Bar 5
+	note D#, 1
+	note __, 1
+	note D_, 1
+	note __, 1
+	note C#, 1
+	note __, 1
+	note D_, 1
+	note __, 1
+	octave 4
+	note G_, 1
+	note __, 1
+	note F_, 1
+	note __, 1
+	note E_, 1
+	note __, 1
+	note F_, 1
+	note __, 1
+	octave 5
+;Bar 6
+	note F_, 1
+	note __, 1
+	note G_, 1
+	note __, 1
+	note G#, 1
+	note __, 1
+	note G_, 1
+	note __, 1
+	note F_, 1
+	note __, 1
+	octave 4
+	note G#, 1
+	note __, 1
+	note G_, 1
+	note __, 1
+	note F_, 1
+	note __, 1
+	octave 5
+;Bar 7
+	note D_, 1
+	note __, 1
+	note D#, 1
+	note __, 1
+	note F_, 1
+	note __, 1
+	note D#, 1
+	note __, 1
+	note D_, 1
+	note __, 1
+	octave 4
+	note F#, 1
+	note __, 1
+	note F_, 1
+	note __, 1
+	note D#, 1
+	note __, 1
+;Bar 8
+	note D#, 1
+	note __, 1
+	note D_, 1
+	note __, 1
+	note C#, 1
+	note __, 1
+	note D_, 1
+	note __, 1
+	note G_, 1
+	note __, 1
+	note F_, 1
+	note __, 1
+	octave 5
+	note C_, 1
+	note __, 1
+	octave 4
+	note A#, 1
+	note __, 1
+	octave 5
+;Bar 9
+	note D#, 1
+	note __, 1
+	note D_, 1
+	note __, 1
+	note C#, 1
+	note __, 1
+	note D_, 1
+	note __, 1
+	note G_, 1
+	note __, 1
+	note F_, 1
+	note __, 1
+	note E_, 1
+	note __, 1
+	note G_, 1
+	note __, 1
+;Bar 10
+	note F_, 1
+	note __, 3
+	note F_, 1
+	note __, 3
+	note E_, 1
+	note __, 1
+	note E_, 1
+	note __, 3
+	note F_, 1
+	note __, 3
+;Bar 11
+	octave 4
+	note F_, 1
+	note __, 1
+	octave 5
+	note F_, 4
+	note __, 2
+	octave 4
+	note F_, 1
+	note __, 1
+	note G_, 1
+	note __, 1
+	note A_, 1
+	note __, 1
+	loopchannel 0, Music_LookLass_Ch1_loop
+
+; ============================================================================================================
 
 Music_LookLass_Ch2:
-	stereopanning $f0
-	dutycycle $0
-	notetype $c, $83
-	note __, 16
-Music_LookLass_branch_ed7d5:
-	intensity $92
+	dutycycle $1
+	notetype 12, $a7
+;Bar 1
+Music_LookLass_Ch2_loop:
+	intensity $b7
+	note __, 2
+	octave 1
+	intensity $f7
+	note F_, 1
+	note __, 1
 	octave 2
+	note F_, 5
+	note __, 1
+	octave 1
+	note F_, 1
+	note __, 1
+	octave 2
+	note F_, 1
+	note __, 1
+	octave 1
+	intensity $e7
+	note F_, 1
+	note __, 1
+;Bar 2
+	intensity $f7
+	note A#, 1
+	note __, 1
+	intensity $e7
+	note A#, 1
+	note __, 7
+	intensity $f7
+	note G_, 1
+	note __, 1
+	octave 2
+	note F_, 2
+	note __, 2
+;Bar 3
+	note C_, 1
+	note __, 1
+	intensity $e7
+	note C_, 1
+	note __, 7
+	octave 1
+	intensity $f7
+	note C_, 1
+	note __, 1
+	octave 2
+	note C_, 2
+	note __, 2
+;Bar 4
+	octave 1
+	note F_, 1
+	note __, 1
+	intensity $e7
+	note F_, 1
+	note __, 3
+	note F_, 1
+	note __, 1
+	intensity $f7
+	note A_, 1
+	note __, 1
+	intensity $e7
+	note A_, 1
+	note __, 3
+	note A_, 1
+	note __, 1
+;Bar 5
+	intensity $f7
+	note A#, 1
+	note __, 1
+	intensity $e7
+	note A#, 1
+	note __, 3
+	intensity $f7
+	note A#, 4
+	intensity $e7
+	note A#, 1
+	note __, 1
+	intensity $f7
+	note D_, 1
+	note __, 1
+	intensity $e7
+	note F_, 1
+	note __, 1
+;Bar 6
+	intensity $f7
 	note B_, 4
-	intensity $91
-	note B_, 4
-	note B_, 4
+	note __, 2
+	intensity $e7
 	note B_, 2
-	intensity $93
-	octave 3
-	note G#, 2
-	loopchannel 0, Music_LookLass_branch_ed7d5
+	intensity $f7
+	note G_, 4
+	note __, 2
+	intensity $e7
+	note G_, 2
+;Bar 7
+	octave 2
+	intensity $f7
+	note C_, 4
+	note __, 2
+	intensity $e7
+	note C_, 2
+	octave 1
+	intensity $f7
+	note D#, 4
+	note __, 2
+	intensity $e7
+	note D#, 2
+;Bar 8
+	intensity $f7
+	note F_, 1
+	note __, 1
+	intensity $e7
+	note F_, 1
+	note __, 13
+;Bar 9
+	intensity $f7
+	note F_, 1
+	note __, 1
+	intensity $e7
+	note F_, 1
+	note __, 13
+;Bar 10
+	octave 2
+	intensity $f7
+	note C_, 1
+	note __, 3
+	intensity $e7
+	note C_, 1
+	note __, 3
+	octave 1
+	intensity $f7
+	note B_, 1
+	note __, 1
+	intensity $e7
+	note B_, 1
+	note __, 3
+	octave 2
+	note C_, 1
+	note __, 3
+;Bar 11
+	octave 1
+	intensity $f7
+	note F_, 1
+	note __, 1
+	octave 2
+	note F_, 4
+	note __, 2
+	octave 1
+	note F_, 1
+	note __, 1
+	octave 2
+	note F_, 1
+	note __, 1
+	octave 1
+	intensity $e7
+	note F_, 1
+	note __, 1
+	loopchannel 0, Music_LookLass_Ch2_loop
+
+; ============================================================================================================
 
 Music_LookLass_Ch3:
-	notetype $c, $10
-	note __, 8
-	octave 6
-	note E_, 2
-	note D#, 2
-	note C#, 2
-	note C_, 2
-Music_LookLass_branch_ed7ef:
-	octave 5
-	note B_, 2
+	notetype 12, $10
+;Bar 1
+Music_LookLass_Ch3_loop:
+	intensity $10
 	note __, 2
-	octave 6
-	note D#, 2
-	note __, 2
-	note E_, 2
+	octave 3
+	note F_, 2 ; WARNING: Auto-Sync says: Rounded up!
+	octave 4
+	note F_, 6
 	note __, 2
 	note D#, 2
-	note __, 2
-	note C#, 2
-	note C_, 2
-	octave 5
-	note B_, 2
-	note A_, 2
-	note G#, 2
-	note A_, 2
-	note A#, 2
-	octave 6
-	note C#, 2
-	octave 5
-	note B_, 2
-	note __, 2
-	octave 6
-	note C#, 2
-	note __, 2
-	octave 5
-	note B_, 2
-	note __, 2
-	note A_, 2
-	note __, 2
-	note G#, 2
-	note F#, 2
-	note E_, 2
-	note D#, 2
-	note E_, 2
-	note F#, 2
-	note G#, 2
-	note A_, 2
-	loopchannel 0, Music_LookLass_branch_ed7ef
-
-Music_LookLass_Ch4:
-	togglenoise $4
-	notetype $c
-	note C_, 16
-Music_LookLass_branch_ed81f:
-	notetype $6
-	note D_, 8
-	note C#, 8
-	note C#, 8
-	note C#, 4
+	note __, 2 ; WARNING: Auto-Sync says: Rounded up!
+;Bar 2
+	intensity $20
 	note D_, 1
+	note __, 1
+	intensity $10
+	note D_, 1
+	note __, 2
+	intensity $20
+	note C#, 1 ; WARNING: Auto-Sync says: Rounded up!
+	intensity $10
+	note D_, 4
+	octave 3
+	note G_, 2
+	octave 4
+	note G#, 4 ; WARNING: Auto-Sync says: Rounded up!
+;Bar 3
+	intensity $20
+	note D#, 1
+	note __, 1
+	intensity $10
+	note D#, 1
+	note __, 3
+	note F_, 4
+	notetype 6, $10
+	octave 3
+	note B_, 1
+	octave 4
+	note C_, 3
+	notetype 12, $20
+	intensity $10
+	note G_, 2
+	note __, 2
+;Bar 4
+	octave 3
+	note F_, 1
+	note __, 1
+	note C_, 2
+	octave 4
+	note D#, 2
+	note __, 2
+	notetype 6, $10
+	octave 3
+	note B_, 1
+	octave 4
+	intensity $10
+	note C_, 3
+	notetype 12, $10
+	octave 3
+	note C_, 2
+	octave 4
+	note F_, 2
+	octave 3
+	note A_, 2
+;Bar 5
+	octave 4
+	note C_, 1
+	note __, 1
+	octave 3
+	note A#, 1
+	note __, 1
+	note D_, 1
+	note __, 1
+	octave 4
+	note F_, 4
+	octave 3
+	intensity $20
+	note A#, 1
+	note __, 1
+	octave 4
+	intensity $10
 	note C#, 1
-	note C#, 1
-	note C#, 1
-	loopchannel 0, Music_LookLass_branch_ed81f
+	note __, 1
+	note D_, 1
+	note __, 1
+;Bar 6
+	octave 3
+	note B_, 2
+	note G_, 2
+	octave 4
+	note D#, 1
+	intensity $20
+	notetype 6, $20
+	note E_, 1
+	note F_, 9
+	notetype 12, $20
+	note D_, 2
+	note C_, 2
+	octave 3
+	intensity $20
+	note B_, 2
+;Bar 7
+	octave 4
+	intensity $10
+	note F_, 2
+	note D#, 2
+	note D_, 1
+	note __, 1
+	note D#, 3
+	note __, 1
+	note D#, 3
+	note __, 1
+	octave 3
+	note A#, 1
+	note __, 1
+;Bar 8
+	note F_, 1
+	note __, 1
+	intensity $20
+	note F_, 1
+	note __, 3
+	octave 4
+	intensity $10
+	note D_, 2
+	note __, 2
+	note D#, 1
+	note __, 1
+	note F_, 1
+	note __, 1
+	intensity $20
+	note A#, 1
+	note __, 1
+;Bar 9
+	notetype 6, $20
+	octave 5
+	intensity $10
+	note D_, 1
+	note __, 3
+	note D_, 1
+	note __, 3
+	octave 4
+	notetype 12, $20
+	intensity $20
+	note E_, 1
+	note __, 1
+	intensity $10
+	note F_, 4
+	octave 3
+	note F_, 1
+	notetype 6, $10
+	note __, 3
+	octave 4
+	note D_, 1
+	notetype 12, $10
+	note __, 1
+	note F_, 1
+	note __, 1
+;Bar 10
+	octave 5
+	note C_, 1
+	note __, 3
+	note C_, 1
+	note __, 3
+	octave 4
+	note B_, 1
+	note __, 1
+	note B_, 1
+	note __, 2
+	intensity $20
+	note G#, 1
+	octave 5
+	intensity $10
+	note C_, 1
+	note __, 3
+;Bar 11
+	octave 4
+	note F_, 1
+	note __, 1
+	octave 3
+	intensity $20
+	note A_, 2
+	octave 5
+	intensity $10
+	note F_, 4
+	octave 3
+	note F_, 1
+	note __, 1
+	octave 4
+	note D#, 2
+	note __, 2
+	loopchannel 0, Music_LookLass_Ch3_loop
+
+; ============================================================================================================
+
