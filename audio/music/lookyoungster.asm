f1d8a01ecac6cb300ed965734d14a7877845668c
U-LAPTOP-CDPSN53K\mattg <mattgmercier@gmail.com>
2022-11-27 20:34:30 -0600

    New Youngster Encounter Theme

--- audio/music/lookyoungster.asm
+++ audio/music/lookyoungster.asm
@@ -1,295 +1,341 @@
+;Coverted using MIDI2ASM
+;Code by TriteHexagon
+;Version 5.0.1 (7-Feb-2021)
+;Visit github.com/TriteHexagon/Midi2ASM-Converter for up-to-date versions.
+
+; ============================================================================================================
+
 Music_LookYoungster:
 	musicheader 3, 1, Music_LookYoungster_Ch1
 	musicheader 1, 2, Music_LookYoungster_Ch2
 	musicheader 1, 3, Music_LookYoungster_Ch3
 
 Music_LookYoungster_Ch1:
-	tempo 118
 	volume $77
-	stereopanning $f
-	dutycycle $3
-	notetype $c, $a3
+	dutycycle $2
+	notetype 12, $a7
+	tempo 119
+;Bar 1
+	octave 4
+	intensity $b7
+	note D_, 1
+	intensity $a7
+	note E_, 1
+	intensity $b7
+	note D_, 1
+	intensity $a7
+	note C_, 1
 	octave 3
-	note G_, 1
-	note G#, 1
+	intensity $b7
+	note B_, 1
+	octave 4
+	intensity $a7
+	note C_, 1
+	octave 3
+	intensity $b7
+	note B_, 1
+	intensity $a7
 	note A_, 1
-	note A#, 1
-	intensity $3e
-	note B_, 16
-	intensity $c3
-	octave 4
-	note C_, 1
-	note __, 3
-	note C_, 4
-	note __, 2
-	note C_, 6
-	intensity $b3
-Music_LookYoungster_branch_f66ea:
-	note __, 2
-	octave 3
 	note G_, 1
-	note __, 1
+	intensity $97
+	note A_, 1
+	intensity $a7
 	note B_, 1
-	note __, 1
-	note G_, 1
-	note __, 1
 	octave 4
+	intensity $97
+	note C_, 1
+	intensity $a7
+	note D_, 1
+	intensity $97
+	note E_, 1
+	intensity $a7
+	note F_, 1
+	intensity $97
+	note F#, 1
+;Bar 2
+Music_LookYoungster_Ch1_loop:
+	notetype 12, $a7
+	intensity $b7
+	note G_, 1 ; WARNING: Auto-Sync says: Rounded down!
+	note __, 1 ; WARNING: Auto-Sync says: Rounded up!
+	note F#, 1
+	note __, 1
+	note E_, 1
+	note __, 1
+	intensity $a7
+	note D_, 1
+	note __, 1
+	intensity $b7
+	note E_, 1
+	note __, 1
+	intensity $a7
 	note D_, 1
 	note __, 1
 	note C_, 1
-	note __, 3
-	note C_, 1
 	note __, 1
-	note __, 16
-	note __, 2
 	octave 3
-	note G_, 1
-	note __, 1
 	note B_, 1
 	note __, 1
-	note G_, 1
+;Bar 3
+	intensity $b7
+	note A_, 1
+	note __, 1
+	intensity $a7
+	note B_, 1
 	note __, 1
 	octave 4
+	intensity $b7
+	note C_, 1
+	note __, 1
+	note D_, 1
+	note __, 3
 	note D_, 1
 	note __, 1
-	note C_, 1
-	note __, 3
-	note C_, 1
+	intensity $a7
+	note E_, 1
 	note __, 1
-	note __, 16
-	loopchannel 0, Music_LookYoungster_branch_f66ea
+	intensity $b7
+	note F#, 1
+	note __, 1
+;Bar 4
+	note G_, 1
+	intensity $a7
+	note G_, 1
+	intensity $b7
+	note G_, 1
+	note __, 1
+	note E_, 1
+	note __, 1
+	note G_, 1
+	note __, 1
+	intensity $a7
+	note F#, 1
+	note __, 1
+	intensity $b7
+	note G_, 1
+	note __, 1
+	intensity $a7
+	note A_, 1
+	note __, 1
+	intensity $b7
+	note D_, 1
+	intensity $a7
+	note D_, 1
+;Bar 5
+	intensity $b7
+	note D_, 1
+	note __, 1
+	octave 3
+	note B_, 1
+	note __, 1
+	octave 4
+	note F#, 1
+	note __, 1
+	notetype 4, $a7
+	octave 3
+	note B_, 2
+	octave 4
+	intensity $97
+	note E_, 2
+	octave 3
+	intensity $a7
+	note B_, 2
+	notetype 6, $a7
+	octave 4
+	intensity $97
+	note E_, 1
+	octave 3
+	note B_, 1
+	octave 4
+	intensity $77
+	note E_, 1
+	octave 3
+	intensity $87
+	note B_, 1
+	octave 4
+	intensity $67
+	note E_, 1
+	octave 3
+	intensity $77
+	note B_, 1
+	octave 4
+	intensity $57
+	note E_, 1
+	note __, 9
+	loopchannel 0, Music_LookYoungster_Ch1_loop
 
-	db $ff
+; ============================================================================================================
 
 Music_LookYoungster_Ch2:
-	stereopanning $f0
-	vibrato $12, $26
 	dutycycle $1
-	notetype $c, $a3
+	notetype 12, $a7
+;Bar 1
+	intensity $57
+	note __, 16
+;Bar 2
+Music_LookYoungster_Ch2_loop:
 	octave 3
+	intensity $c7
 	note B_, 1
 	octave 4
+	intensity $b7
 	note C_, 1
-	note C#, 1
+	intensity $c7
 	note D_, 1
-	intensity $3e
-	notetype $c, $2e
-	note G_, 16
-	notetype $c, $b3
-	intensity $c3
-	note F_, 1
-	note __, 3
-	note F_, 4
+	intensity $b7
+	note C_, 1
+	octave 3
+	intensity $c7
+	note B_, 1
+	note __, 1
+	intensity $b7
+	note A_, 1
+	note __, 1
+	intensity $c7
+	note B_, 1
+	note __, 1
+	intensity $b7
+	note A_, 1
+	note __, 1
+	intensity $c7
+	note G_, 1
+	note __, 1
+	intensity $b7
+	note E_, 1
+	note __, 1
+;Bar 3
+	intensity $c7
+	note F#, 1
+	note __, 1
+	intensity $b7
+	note G_, 1
+	note __, 1
+	intensity $c7
+	note A_, 1
+	note __, 1
+	note B_, 2
 	note __, 2
-	note F#, 6
-Music_LookYoungster_branch_f672f:
-	dutycycle $3
-	intensity $b3
-	note __, 2
-	octave 3
-	note B_, 1
-	note __, 1
-	octave 4
-	note D_, 1
-	note __, 1
-	octave 3
-	note B_, 1
-	note __, 1
-	octave 4
 	note G_, 1
 	note __, 1
-	note F_, 1
-	note __, 3
-	note F_, 1
+	intensity $b7
+	note F#, 1
 	note __, 1
-	intensity $97
-	dutycycle $2
-	note D#, 1
-	note F_, 1
-	note D#, 1
-	note D_, 1
+	intensity $c7
+	note E_, 1
+	note __, 1
+;Bar 4
+	note E_, 1
+	note __, 1
+	intensity $b7
 	note C_, 1
 	note __, 1
-	octave 3
-	note G#, 1
-	note __, 3
-	note F_, 1
-	note __, 1
+	octave 2
+	intensity $c7
 	note G_, 1
 	note __, 1
-	note G#, 1
+	octave 3
+	intensity $b7
+	note E_, 1
 	note __, 1
-	dutycycle $3
-	intensity $b3
-	note __, 2
-	note B_, 1
-	note __, 1
-	octave 4
+	intensity $c7
 	note D_, 1
 	note __, 1
-	octave 3
+	intensity $b7
+	note F#, 1
+	note __, 1
+	intensity $c7
+	note A_, 1
+	note __, 1
+	intensity $b7
+	note D_, 1
+	note __, 1
+;Bar 5
+	intensity $c7
+	note D_, 1
+	note __, 1
+	octave 2
+	intensity $b7
 	note B_, 1
 	note __, 1
-	octave 4
-	note G_, 1
-	note __, 1
-	note F_, 1
-	note __, 3
-	note F_, 1
-	note __, 1
-	dutycycle $2
-	intensity $97
-	note G#, 1
-	note A#, 1
-	note G#, 1
-	note G_, 1
-	note F_, 1
-	note __, 1
-	note C_, 1
-	note __, 3
 	octave 3
-	note G#, 1
+	intensity $c7
+	note F#, 1
 	note __, 1
-	note A#, 1
-	note __, 1
-	octave 4
-	note C_, 1
-	note __, 1
-	loopchannel 0, Music_LookYoungster_branch_f672f
+	intensity $b7
+	note E_, 4
+	note __, 6
+	loopchannel 0, Music_LookYoungster_Ch2_loop
 
-	db $ff
+; ============================================================================================================
 
 Music_LookYoungster_Ch3:
-	stereopanning $ff
-	vibrato $2, $24
-	notetype $c, $25
-	note __, 4
-	octave 2
-	note G_, 1
-	note __, 1
+	notetype 12, $10
+;Bar 1
+	octave 4
+	intensity $10
+	note D_, 4
 	octave 3
-	note G_, 1
-	note __, 1
-	octave 2
-	note G_, 1
-	note __, 1
-	octave 3
-	note G_, 1
-	note __, 1
-	octave 2
-	note G_, 1
-	note __, 1
-	octave 3
-	note G_, 1
-	note __, 1
-	intensity $15
-	octave 2
-	note G_, 1
-	note __, 1
-	octave 3
-	note G_, 1
-	note __, 1
+	note A_, 2
+	octave 4
 	note D_, 1
 	note __, 3
+	octave 3
+	note A_, 6
+;Bar 2
+Music_LookYoungster_Ch3_loop:
+	note G_, 4
+	octave 4
+	note D_, 1
+	note __, 1
+	octave 3
+	notetype 6, $10
+	note G_, 1
+	note __, 3
+	octave 4
+	notetype 12, $10
+	note C_, 4
+	note G_, 1
+	note __, 1
+	note C_, 1
+	note __, 1
+;Bar 3
 	note D_, 4
-	note __, 2
-	note G#, 6
-	intensity $25
-Music_LookYoungster_branch_f67ae:
-	octave 2
+	note A_, 1
+	note __, 1
+	note E_, 3
+	note __, 1
+	note E_, 2
+	notetype 6, $10
+	note B_, 4
+	note E_, 3
+	note __, 1
+	notetype 12, $10
+;Bar 4
+	note C_, 4
 	note G_, 1
+	note __, 1
+	note C_, 1
+	note __, 1
+	note D_, 2
+	note A_, 2
+	note D_, 1
 	note __, 1
 	octave 3
-	note G_, 1
+	note B_, 4
+;Bar 5
+	octave 4
+	note D_, 2
+	note A_, 1
 	note __, 1
-	octave 2
-	note G_, 1
+	notetype 6, $10
+	note E_, 7
 	note __, 1
+	notetype 12, $10
+	note E_, 2
 	octave 3
-	note G_, 1
-	note __, 1
-	octave 2
-	note G_, 1
-	note __, 1
-	octave 3
-	note G_, 1
-	note __, 1
-	octave 2
-	note G_, 1
-	note __, 1
-	octave 3
-	note G_, 1
-	note __, 1
-	octave 2
-	note F_, 1
-	note __, 1
-	octave 3
-	note D#, 1
-	note __, 1
-	octave 2
-	note F_, 1
-	note __, 1
-	octave 3
-	note D#, 1
-	note __, 1
-	octave 2
-	note F_, 1
-	note __, 1
-	octave 3
-	note D#, 1
-	note __, 1
-	octave 2
-	note F_, 1
-	note __, 1
-	octave 3
-	note D#, 1
-	note __, 1
-	octave 2
-	note G_, 1
-	note __, 1
-	octave 3
-	note G_, 1
-	note __, 1
-	octave 2
-	note G_, 1
-	note __, 1
-	octave 3
-	note G_, 1
-	note __, 1
-	octave 2
-	note G_, 1
-	note __, 1
-	octave 3
-	note G_, 1
-	note __, 1
-	octave 2
-	note G_, 1
-	note __, 1
-	octave 3
-	note G_, 1
-	note __, 1
-	octave 2
-	note G#, 1
-	note __, 1
-	octave 3
-	note D#, 1
-	note __, 1
-	octave 2
-	note G#, 1
-	note __, 1
-	octave 3
-	note D#, 1
-	note __, 1
-	octave 2
-	note G#, 1
-	note __, 1
-	octave 3
-	note D#, 1
-	note __, 1
-	note G_, 1
-	note __, 1
-	note G#, 1
-	note __, 1
-	loopchannel 0, Music_LookYoungster_branch_f67ae
+	note B_, 2
+	note E_, 2
+	loopchannel 0, Music_LookYoungster_Ch3_loop
 
-	db $ff
+; ============================================================================================================
+
