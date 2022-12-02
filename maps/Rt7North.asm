b23f166b9b2f3d50768492774bab086ff409dcd0
U-LAPTOP-CDPSN53K\mattg <mattgmercier@gmail.com>
2022-12-02 17:12:09 -0600

    Add Twist Mountain warp to Route 7

--- maps/Rt7North.asm
+++ maps/Rt7North.asm
@@ -156,8 +156,9 @@ R7NorthSignText:
 Rt7North_MapEvents:
 	db 0, 0 ; filler
 
-	db 1 ; warp events
+	db 2 ; warp events
 	warp_event  5, 37, R_7_TRADE_HOUSE, 1
+	warp_event 15,  3, TWIST_MOUNTAIN_3F, 1
 
 	db 0 ; coord events
 
